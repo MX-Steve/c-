1ce8d048cc79f410afccc12b427fe27bd481b36c
MX-Steve <2513556525@qq.com>
2017-11-29 10:21:25 +0800

    day04

---
+++ cstep01/day04/02demo.c
@@ -0,0 +1,15 @@
+/*
+ *操作附
+ * */
+#include <stdio.h>
+int main(){
+    int num = 0;
+    char ch = 0;
+    num = ch  = 300;
+    printf("num是%d\n",num);
+    int n = 0;
+    n += 10;
+    n *=2+5;
+    printf("n是%d\n",n);
+    return 0;
+}
